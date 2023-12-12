70099297eecea3fde45ea8d25f241f604807f657
SunriseWasTaken <79020755+SunriseWasTaken@users.noreply.github.com>
2023-12-12 22:45:28 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const reveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+
+  }
+}
+
+reveal();
+addEventOnElem(window, "scroll", reveal);
